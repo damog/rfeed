471905fe43779d13a62da0d4847e6ad7acf97715
David Moreno <david@axiombox.com>
2009-01-10 18:49:14 -0500

    moving forward

--- lib/rfeed.rb
+++ lib/rfeed.rb
@@ -20,35 +20,4 @@ Dir["#{File.expand_path(File.dirname(__FILE__))}/rfeed/*.rb"].each do |r|
 	require "#{r}"
 end
 
-module Rfeed
-	def self.version
-		"0.1"
-	end
-
-	def self.add
-		puts "I will add a new feed"
-	end
-
-	def self.feed_new(url)
-
-		# validamos primero el link
-		# - si no es una URL, error
-		# - verificar que rfeedparser pueda leerlo
-
-		unless url =~ /((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?/
-			puts "'#{url}' doesn't look like a URL"
-			exit 1
-		end
-
-		feed = Feed.new
-		feed.feed_url = url
-
-		unless feed.save
-			puts feed.errors.full_messages
-			exit 1
-		end
-
-	end
-
-end
-
+# TODO: Make sure extend.yml is being used
--- lib/rfeed/models/Feed.rb
+++ lib/rfeed/models/Feed.rb
@@ -5,50 +5,76 @@ require "goodies/goodies/lwr-simple"
 class Feed < ActiveRecord::Base
 	has_many :posts
 
-	validates_presence_of :link, :feed_url
+	validates_presence_of :feed_url, :message => "wasn't found"
+	validates_presence_of :link, :message => "wasn't found on `feed_url'"
+	validates_uniqueness_of :feed_url, :message => "already exists on database"
 
-	def feed=(url)
-		url = LWR::Simple.normalize(url).to_s
+	attr_accessor :fp
 
-		# checking that the url is a feed:
-		feedbag = Feedbag.find(url)
-		if feedbag.size == 1 and feedbag.first == url
-			# si es
-		elsif feedbag.first.nil?
-			$stderr.puts "URL not a feed and not feeds found"
-			return nil
-		else
-			url = feedbag.first
-			$stderr.puts "URL provided not a feed but using `#{url}'"
-		end
+	def parse(url)
+    url = LWR::Simple.normalize(url).to_s
+
+    # checking that the url is a feed:
+    feedbag = Feedbag.find(url)
+    if feedbag.size == 1 and feedbag.first == url
+      # si es
+    elsif feedbag.first.nil?
+      $stderr.puts "URL not a feed and not feeds found"
+      return nil
+    else
+      url = feedbag.first
+      $stderr.puts "URL provided not a feed but using `#{url}'"
+    end
 
 		fp = FeedParser.parse url
 
 		if fp.status.nil? or fp.status >= 400.to_s
-			$stderr.puts "Errors with #{url}!"
+      $stderr.puts "Errors with #{url}!"
+      return nil
+    end
+
+    # there's no real easy way to do this
+    # so we'll only do if the feed_validator gem is available
+    validate = false # assuming it's not there
+    if fp.entries.empty?
+      begin
+        require "feed_validator"
+        validate = true
+      rescue LoadError
+        $stderr.puts "Not using feed_validator"
+      end
+
+      if validate
+        v = W3C::FeedValidator.new
+        v.validate_url url
+        unless v.valid?
+          $stderr.puts "Invalid feed"
+          return nil
+        end
+      end
+    end
+		
+		self.link = fp.feed.link
+		self.title = fp.feed.title
+		self.etag = fp.etag
+		self.last_modified = fp.modified_time
+		self.feed_url = url
+		self.fp = fp
+	end
+
+	def update
+		p = self.parse(self.feed_url)
+
+		p.entries.each do |e|
+#			Post.new  !!!!
+		end
+	end
+
+	def feed=(url)
+		p = self.parse(url)
+
+		if p.nil?
 			return nil
 		end
-
-		# there's no real easy way to do this
-		# so we'll only do if the feed_validator gem is available
-		validate = false # assuming it's not there
-		if fp.entries.empty?
-			begin
-				require "feed_validator"
-				validate = true
-			rescue LoadError
-				$stderr.puts "Not using feed_validator"
-			end
-
-			if validate
-				v = W3C::FeedValidator.new
-				v.validate_url url
-				unless v.valid?
-					$stderr.puts "Invalid feed"
-					return nil
-				end
-			end
-		end
-		self.feed_url = url
 	end
 end

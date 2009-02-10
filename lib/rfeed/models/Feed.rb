3e1d5c50f0dec399fe96a9aa85d5b7c2390a1015
David Moreno <david@axiombox.com>
2009-02-09 21:55:01 -0500

    feed model finally fixed

--- lib/rfeed/models/Feed.rb
+++ lib/rfeed/models/Feed.rb
@@ -3,14 +3,6 @@ require "feedbag"
 require "goodies/goodies/lwr-simple"
 require 'digest/md5'
 
-class FeedParserRFeed < FeedParser
-	def initialize
-	end
-
-	def parse
-	end
-end
-
 class Feed < ActiveRecord::Base
 	has_many :posts
 
@@ -18,14 +10,13 @@ class Feed < ActiveRecord::Base
 	validates_presence_of :link, :message => "wasn't found on `feed_url'"
 	validates_uniqueness_of :feed_url, :message => "already exists on database"
 
-	attr_accessor :fp, :changed
-
-	def name
-		return "#{self.id}:#{self.feed_url}"
-	end
-
-	def parse(url, params = {})
-    url = LWR::Simple.normalize(url).to_s
+	attr_accessor :fp, :url
+	
+	before_validation_on_create :before_created
+	before_validation_on_update :before_updated
+	
+	def before_created
+    self.url = LWR::Simple.normalize(self.url).to_s
 
     # checking that the url is a feed:
     feedbag = Feedbag.find(url)
@@ -33,22 +24,22 @@ class Feed < ActiveRecord::Base
       # si es
     elsif feedbag.first.nil?
       $stderr.puts "URL not a feed and not feeds found"
-      return nil
+      return false
     else
-      url = feedbag.first
+      self.url = feedbag.first
       $stderr.puts "URL provided not a feed but using `#{url}'"
     end
 
-		fp = FeedParser.parse url, { :etag => params[:etag], :modified => params[:modified] }
+    self.feed_url = self.url
+		fp = FeedParser.parse self.url
 
 		if fp.status.nil? or fp.status >= 400.to_s
       $stderr.puts "Errors with #{url}!"
-      return nil
+      return false
     end
 
 		if fp.status == "304" and fp.feed.empty? and fp.entries.empty? # not changed!
-			self.changed = false
-			return self
+			return false
 		end
 
     # there's no real easy way to do this
@@ -64,37 +55,59 @@ class Feed < ActiveRecord::Base
 
       if validate
         v = W3C::FeedValidator.new
-        v.validate_url url
+        v.validate_url self.url
         unless v.valid?
           $stderr.puts "Invalid feed"
-          return nil
+          return false
         end
       end
     end
-		
-		self.link = fp.feed.link
-		self.title = fp.feed.title
-		if params[:new]
-			self.etag = nil
-			self.last_modified = nil
-		else
-			puts "ahh!"
-			self.etag = fp.etag
-			self.last_modified = fp.modified_time
+    
+    self.link = fp.feed.link
+    self.title = fp.feed.title
+    true # if i made it this far, it was successful callback  
+	end
+	
+
+	def name
+		return "#{self.id}:#{self.feed_url}"
+	end
+
+	def before_updated
+	  self.fp = FeedParser.parse self.feed_url, { :etag => self.etag, :modified => self.last_modified }
+	
+		if self.fp.status.nil? or self.fp.status >= 400.to_s
+      $stderr.puts "Errors with #{self.feed_url}!"
+      return false
+    end
+
+		if self.fp.status == "304" and self.fp.feed.empty? and self.fp.entries.empty? # not changed!
+		  $stderr.puts "Feed unchanged (not updating)!"
+			return false
 		end
-		self.feed_url = url
-		self.changed = true
-		self.fp = fp
-		self
+    
+		self.link = self.fp.feed.link
+		self.title = self.fp.feed.title
+		self.etag = self.fp.etag
+		self.last_modified = self.fp.modified_time
+		
+    # TODO: Find a good way to reset the feed_url when changed
+		# self.feed_url =
+		
+		self.fp.entries.each do |e|
+		  post = Post.new :e => e
+		end
+		
+		true 
 	end
 
 	def fetch
 		parse(self.feed_url, :etag => self.etag, :modified => self.last_modified)
 
-		unless self.changed
-			puts "#{self.name}: Feed unchanged"
-			return
-		end
+	  # unless self.changed
+	 #     puts "#{self.name}: Feed unchanged"
+	 #     return
+	 #   end
 
 		self.fp.entries.each do |e|
 			post = Post.entry(e, self)
@@ -110,11 +123,5 @@ class Feed < ActiveRecord::Base
 		end
 	end
 
-	def feed=(url)
-		p = self.parse(url, :new => true)
 
-		if p.nil?
-			return nil
-		end
-	end
 end
